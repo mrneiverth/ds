5a8f666d9da324541d53f2605149f3c370378cb5
Erick Costa Fuga <erick_fuga@hotmail.com>
2018-06-18 02:21:52 -0300

    Acrescentados métodos para criar, deletar e atualizar figurinha.

--- CodeIgniter/application/controllers/Base.php
+++ CodeIgniter/application/controllers/Base.php
@@ -32,14 +32,33 @@ class Base extends CI_Controller{
     * Adiciona uma figurinha.
     */
     function criarFigurinha(){
-        //TODO
+        //$data = ("25483125412", "12345678", "Fulano", 0);
+        $data = $this->input->post();
+        $status = $this->figurinha_model->Inserir($data);
+
+        if(!$status){
+            $this->session->set_flashdata('error', 'Erro ao inserir nova figurinha.');
+        }else{
+            $this->session->set_flashdata('success', 'Figurinha inserida com sucesso.');
+        }
+
+        $this->load->view('home');
     }
 
     /**
     * Remove uma figurinha.
     */
     function deletarFigurinha(){
-        //TODO
+        $id = $this->input->post('ID');
+        $status = $this->figurinha_model->Excluir($id);
+
+        if(!$status){
+            $this->session->set_flashdata('error', 'Erro ao excluir a figurinha.');
+        }else{
+            $this->session->set_flashdata('success', 'Figurinha excluída com sucesso.');
+        }
+
+        $this->load->view('home');
     }
 
     /**
@@ -47,7 +66,7 @@ class Base extends CI_Controller{
     */
     function deletarUsuario(){
 
-        $cpf = $this->input->post();
+        $cpf = $this->input->post('CPF');
         $status = $this->usuario_model->Excluir($cpf);
 
         if(!$status){
@@ -63,7 +82,20 @@ class Base extends CI_Controller{
     * Atualiza uma figurinha.
     */
     function atualizarFigurinha(){
-        //TODO
+        
+        $id = $this->input->post('ID');
+        $status = $this->figurinha_model->GetByID($id);
+
+        $data = $this->input->post();
+        $status = $this->figurinha_model->Atualizar($id, $data);
+
+        if(!$status){
+            $this->session->set_flashdata('error', 'Erro ao atualizar a figurinha.');
+        }else{
+            $this->session->set_flashdata('success', 'Figurinha excluída com sucesso.');
+        }
+
+        $this->load->view('home');
     }
 
     /**
@@ -71,11 +103,11 @@ class Base extends CI_Controller{
     */
     function atualizarUsuario(){
 
-        $cpf = $this->input->post();
+        $cpf = $this->input->post('CPF');
         $status = $this->usuario_model->GetByCPF($cpf);
 
         $data = $this->input->post();
-        $status = $this->usuario_model->Inserir($data);
+        $status = $this->usuario_model->Atualizar($cpf, $data);
 
         if(!$status){
             $this->session->set_flashdata('error', 'Erro ao atualizar o usuário.');
