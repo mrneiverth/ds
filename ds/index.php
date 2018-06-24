759c6373a75412741a771421cdfad7f07230e6dd
Erick Costa Fuga <erick_fuga@hotmail.com>
2018-06-24 01:01:50 -0300

    Polidinha básica na home

--- ds/index.php
+++ ds/index.php
@@ -20,80 +20,75 @@
 
 		<!-- Static navbar -->
 	    <nav class="navbar navbar-default navbar-static-top">
-	      <div class="container">
-	        <div class="navbar-header">
-	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	            <span class="sr-only">Toggle navigation</span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-	          <img src="imagens/" />
-	        </div>
+	      	<div class="container">
+		        <div class="navbar-header">
+			        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			        	<span class="sr-only">Toggle navigation</span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			        </button>
+			        <img src="imagens/" />
+		        </div>
 
-	        <div id="navbar" class="navbar-collapse collapse">
-	          <ul class="nav navbar-nav navbar-right">
-	            <li><a href="inscrevase.php">Inscrever-se</a></li>
-	            <li class="">
-	            	<a id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Entrar</a>
-					<ul class="dropdown-menu" aria-labelledby="entrar">
-						<div class="col-md-12">
-				    		<p>Você possui uma conta?</h3>
-				    		<br />
-							<form method="post" action="" id="formLogin">
-								<div class="form-group">
-									<input type="text" class="form-control" id="campo_cpf" name="cpf" placeholder="CPF" />
+		        <div id="navbar" class="navbar-collapse collapse">
+		          	<ul class="nav navbar-nav navbar-right">
+			            <li><a href="usuario.html">Inscrever-se</a></li>
+			            <li class="">
+			            	<a id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Entrar</a>
+							<ul class="dropdown-menu" aria-labelledby="entrar">
+								<div class="col-md-12">
+						    		<p>Você possui uma conta?</h3>
+						    		<br />
+									<form method="post" action="" id="formLogin">
+										<div class="form-group">
+											<input type="text" class="form-control" id="campo_cpf" name="cpf" placeholder="CPF" />
+										</div>
+										<div class="form-group">
+											<input type="password" class="form-control red" id="campo_senha" name="senha" placeholder="Senha" />
+										</div>
+										<button type="buttom" class="btn btn-primary" id="btn_login">Entrar</button>
+										<br/><br/>
+									</form>
 								</div>
-
-								<div class="form-group">
-									<input type="password" class="form-control red" id="campo_senha" name="senha" placeholder="Senha" />
-								</div>
-
-								<button type="buttom" class="btn btn-primary" id="btn_login">Entrar</button>
-
-								<br /><br />
-
-							</form>
-						</form>
-				  	</ul>
-	            </li>
-	          </ul>
-	        </div><!--/.nav-collapse -->
-	      </div>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
 	    </nav>
 
 
 	    <div class="container">
 
-	      <!-- Main component for a primary marketing message or call to action -->
-	      <div class="jumbotron">
-	        <h1>Bem vindo ao sistema de trocas de fig</h1>
-	        <p>Troque figs!</p>
-	      </div>
-				<div class="col-md-4">
-					<form method="get" action="show_figs.php" id="showFigs">
-						<div class="form-group">
-							<button type="submit" class="btn btn-primary form-control">Show All Figs</button>
-						</div>
-				</div>
-	      <div class="clearfix"></div>
+		    <!-- Main component for a primary marketing message or call to action -->
+		    <div class="jumbotron">
+		    	<h1 align="center">Bem vindo ao sistema de trocas de figurinhas!</h1>
+		    </div>
+			<div class="col-md-4">
+				<form method="get" action="figurinha.html" id="showFigs">
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary form-control">Figurinhas</button>
+					</div>
+				</form>
+			</div>
+			<div class="col-md-4">
+				<form method="get" action="troca.html" id="showTrocas">
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary form-control">Trocas</button>
+					</div>
+				</form>
+			</div>
+			<div class="col-md-4">
+				<form method="get" action="read_usuario_view.php" id="showUsers">
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary form-control">Usuarios</button>
+					</div>
+				</form>
+			</div>
+
 		</div>
-			<div class="col-md-4">
-				<form method="get" action="show_trocas.php" id="showTrocas">
-					<div class="form-group">
-						<button type="submit" class="btn btn-secondary form-control">Show All trocas</button>
-					</div>
-			</div>
-			<div class="col-md-4">
-				<form method="get" action="show_users.php" id="showUsers">
-					<div class="form-group">
-						<button type="submit" class="btn btn-thirdy form-control">Show All Users</button>
-					</div>
-			</div>
-
-
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 	</body>
 </html>
-
